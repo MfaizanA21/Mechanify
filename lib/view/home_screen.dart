8c8a3fde6c94747d5f73547bb39d107b406c687e
uzarnaam <mfaizaan771@gmail.com>
2023-03-19 23:46:35 +0500

    Home Screen is almost finished

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  Home({super.key});
+  const Home({super.key});
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      drawer:const Drawer(
+
+
+      ),
       appBar: AppBar(
         title: const Text('Mechanify'),
         centerTitle: true,
@@ -13,29 +17,118 @@ class Home extends StatelessWidget {
               gradient: LinearGradient(
                   begin: Alignment.centerLeft,
                   end: Alignment.centerRight,
-                  colors: <Color>[Colors.black, Colors.teal]
+                  colors: <Color>[Colors.black45, Colors.teal]
               )
           ),
         ),
         titleTextStyle: const TextStyle(
           fontStyle: FontStyle.italic,
-          fontFamily: 'Courier',
+          fontFamily: 'Ariel',
           fontWeight: FontWeight.bold,
           color: Colors.black,
           fontSize: 42.0,
         ),
-
-        leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.menu_rounded,color: Colors.black,),
-
-                onPressed: () {Scaffold.of(context).openDrawer(); },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            }
-        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.car_repair_rounded, color: Colors.black,
+            ),
+            onPressed: () {
+            },
+          )
+        ],
       ),
+      body: Column (
+        crossAxisAlignment: CrossAxisAlignment.start,
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          //Ink.image(image: AssetImage('assets/images/res_home.png')),
+           const Image(image: AssetImage('assets/images/res_home.png')),
+         Container(
+           padding: const EdgeInsets.all(6),
+           color: Colors.teal,
+           child: Row(
+             children:  const <Widget>[
+               Expanded(
+                   child:  Text('OUR SERVICES', style: TextStyle(
+                     color: Colors.black,
+                     fontSize: 32,
+                     fontFamily: 'Ariel',
+                     fontStyle: FontStyle.italic,
+                     fontWeight: FontWeight.bold,
+                   ),
+                     textAlign: TextAlign.start,
+                   ),
+
+               )
+             ]
+           ),
+         ),
+           Row(
+             children: const [
+             //Image(image: AssetImage('assets/images/car-wash.png')),
+               InsertButton('Engine Service', Image(image: AssetImage('assets/images/res_home.png'))),
+               InsertButton("Car Wash", Image(image: AssetImage('assets/images/car-wash.png'))),
+            ]
+           ),
+          Row(
+              children: const [
+                InsertButton("Oil Change", Image(image: AssetImage('assets/images/oil.png'))),
+                InsertButton("paint",   Image(image: AssetImage('assets/images/spray-gun.png'))),
+              ]
+          ),
+          Row(
+              children: const [
+                InsertButton("Accessories",  Image(image:AssetImage('assets/images/car-battery.png'))),
+                InsertButton("Tyre Change",  Image(image:AssetImage('assets/images/rim.png'))),
+              ]
+          ),
+
+        ],
+      ),
+    backgroundColor: Colors.black,
     );
   }
 }
+
+class InsertButton extends StatelessWidget{
+  final String? name;
+  final Image? image;
+  const InsertButton(this.name,this.image, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4),
+      width: 194,
+      height: 130,
+      child: ElevatedButton(
+        onPressed: () {},
+
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black87,
+          fixedSize: const Size(70, 30),
+          side: const BorderSide(color: Colors.teal),
+          //side: BorderSide(width: 6, color: Colors.black),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          )
+      ),
+        //Image(image: AssetImage('$image'))
+
+        child: Text('$name',
+          style: const TextStyle(
+            fontFamily: 'Ariel',
+            fontStyle: FontStyle.italic,
+            fontWeight:FontWeight.normal,
+            color: Colors.teal
+          ),
+        ),
+
+
+      ),
+      );
+  }
+}
+
+
+
---
+++ lib/view/mechanics.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class  MechanicList extends StatelessWidget {
+  const MechanicList ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
